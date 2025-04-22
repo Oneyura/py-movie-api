dd2f52e536ab675960e05dacb438aa18f0eb952d
Oneyura <hryn.yuri@gmail.com>
2025-04-22 20:36:33 +0300

    create models and admin panel

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "movies"
+
+    def __str__(self):
+        return f"{self.title} {str(self.duration)} minutes"
